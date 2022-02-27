b861ec566f98329e895df87a191e11197b653c84
deveki-gmail <devekinandan@gmail.com>
2022-02-27 18:40:00 +0530

    code implemented

--- src/main/java/com/example/greeting/server/GreetingServerApplication.java
+++ src/main/java/com/example/greeting/server/GreetingServerApplication.java
@@ -1,13 +1,46 @@
 package com.example.greeting.server;
 
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GreetingServerApplication {
 
+	static Map<Integer, String> map  = new HashMap<Integer, String>();
+	
+	static{
+		map.put(Integer.valueOf(1), "Good Morning");
+		map.put(Integer.valueOf(2), "Good After Noon");
+		map.put(Integer.valueOf(3), "Good Evening");
+		map.put(Integer.valueOf(4), "Good Night");
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingServerApplication.class, args);
 	}
-
+	
+	
+	@GetMapping("/greet")
+	public String hello(){
+		LocalTime time = LocalTime.now();
+		int hour = time.getHour();
+		if(hour < 12){
+			return map.get(Integer.valueOf(1));
+		}
+		if(hour > 12 && hour < 18){
+			return map.get(Integer.valueOf(2));
+		}
+		if(hour > 18){
+			return map.get(Integer.valueOf(3));
+		}
+		return map.get(Integer.valueOf(3));
+	}
 }
